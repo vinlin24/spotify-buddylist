162a31fad7d423343d6f22c51d7944a46b7f9e1a
Vincent Lin <vinlin24@outlook.com>
2022-10-24 02:05:10 -0700

    Fix 3.10 type hints for 3.8 compatibility

--- src/spotify_buddylist/async_api.py
+++ src/spotify_buddylist/async_api.py
@@ -4,14 +4,14 @@ Defines the asynchronous API functions for this package.
 """
 
 from asyncio import AbstractEventLoop, get_event_loop
-from typing import List
+from typing import List, Optional
 
 from .models import AccessFields, FriendActivity
 from .sync_api import get_friend_activity, get_web_access_token
 
 
 async def fetch_web_access_token(sp_dc_cookie: str,
-                                 loop: AbstractEventLoop | None = None,
+                                 loop: Optional[AbstractEventLoop] = None,
                                  ) -> AccessFields:
     """Return access credentials from get_access_token endpoint."""
     loop = loop or get_event_loop()
@@ -23,7 +23,7 @@ async def fetch_web_access_token(sp_dc_cookie: str,
 
 
 async def fetch_friend_activity(access_token: str,
-                                loop: AbstractEventLoop | None = None
+                                loop: Optional[AbstractEventLoop] = None
                                 ) -> List[FriendActivity]:
     """Return the friend activities as a list of dataclasses."""
     loop = loop or get_event_loop()
