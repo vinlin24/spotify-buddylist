23e2f7f2ef95252c0c3d08fb3b05d205ae890db0
Vincent Lin <vinlin24@outlook.com>
2022-10-23 19:59:47 -0700

    Rename some things so they can be imported

---
+++ src/spotify_buddylist/__init__.py
@@ -0,0 +1,11 @@
+"""__init__.py
+
+Initialize the API namespace.
+"""
+
+from .async_api import fetch_friend_activity, fetch_web_access_token
+from .models import *
+from .sync_api import get_friend_activity, get_web_access_token
+
+__author__ = "Vincent Lin"
+__version__ = "0.0.0"
