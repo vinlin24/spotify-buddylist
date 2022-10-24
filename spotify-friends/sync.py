bd55b597cebe3ca6af99384572b4caf0d89deecc
Vincent Lin <vinlin24@outlook.com>
2022-10-23 19:30:34 -0700

    Finish synchronous API functions

--- spotify-friends/sync.py
+++ spotify-friends/sync.py
@@ -3,11 +3,13 @@
 Defines the synchronous API functions for this package.
 """
 
-from datetime import datetime
+from datetime import datetime, timezone
+from typing import Any, Dict, List
 
 import requests
 
-from .models import AccessFields
+from .models import (AccessFields, Album, Artist, Context, FriendActivity,
+                     Track, User)
 
 
 def get_web_access_token(sp_dc_cookie: str) -> AccessFields:
@@ -26,18 +28,47 @@ def get_web_access_token(sp_dc_cookie: str) -> AccessFields:
     )
 
     # Process the JSON data
-    data = response.json()
-    client_id = data["clientId"]
-    access_token = data["accessToken"]
-    expiration_dt = datetime.fromtimestamp(
-        int(data["accessTokenExpirationTimestampMs"]) / 1000
-    )
-    is_anonymous = data["isAnonymous"]
+    data: Dict[str, Any] = response.json()
+    # Convert millisecond timestamp to datetime
+    timestamp_ms: int = data["accessTokenExpirationTimestampMs"]
+    timestamp_dt = datetime.fromtimestamp(timestamp_ms / 1000, timezone.utc)
+    data["accessTokenExpirationTimestampMs"] = timestamp_dt
 
     # Construct and return the dataclass
-    return AccessFields(
-        client_id=client_id,
-        access_token=access_token,
-        access_token_expiration=expiration_dt,
-        is_anonymous=is_anonymous,
+    return AccessFields(**data)
+
+
+def _make_friend_activity(friend: Dict[str, Any]) -> FriendActivity:
+    """Convert one JSON activity object into a FriendActivity."""
+    user = User(**friend["user"])
+    track = Track(
+        uri=friend["track"]["uri"],
+        name=friend["track"]["name"],
+        imageUrl=friend["track"]["imageUrl"],
+        album=Album(**friend["track"]["album"]),
+        artist=Artist(**friend["track"]["artist"]),
+        context=Context(**friend["track"]["context"]),
     )
+    return FriendActivity(
+        timestamp=friend["timestamp"],
+        user=user,
+        track=track,
+    )
+
+
+def get_friend_activity(access_token: str) -> List[FriendActivity]:
+    """Return the friend activities as a list of dataclasses."""
+    # Make the web request
+    response = requests.get(
+        url="https://guc-spclient.spotify.com/presence-view/v1/buddylist",
+        headers={
+            "Authorization": f"Bearer {access_token}",
+        },
+        timeout=4.0,
+    )
+
+    # Process the JSON data
+    friends: List[Dict[str, Any]] = response.json()["friends"]
+
+    # Construct and return the dataclasses
+    return [_make_friend_activity(d) for d in friends]
